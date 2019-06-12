4857a0d3b08061bb5b7fdcad213f3e45754010e2
Illia1927 <wides544@gmail.com>
2019-06-12 17:20:30 +0300

    add different junit tests

--- src/com/practice/myHashMap/MyHashMapImplTest.java
+++ src/com/practice/myHashMap/MyHashMapImplTest.java
@@ -1,6 +1,8 @@
 package com.practice.myHashMap;
 
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -28,5 +30,38 @@ public class MyHashMapImplTest {
 
         //3. Test equal for getting value
         assertThat(myHashMap.get(2), is(expected.get(2)));
+    }
+
+    MyHashMap<Integer, Long> expectedTestMap = new MyHashMapImpl<>();
+    MyHashMap<Integer, Long> actualTestMap;
+
+    @Before
+    public void initialization() {
+        expectedTestMap.put(1, 50L);
+    }
+
+    @Test
+    public void putTest() {
+        actualTestMap = new MyHashMapImpl<>();
+        actualTestMap.put(1, 50L);
+
+        Integer expected = 50;
+        Integer actual = Math.toIntExact(actualTestMap.get(1));
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void getTest() {
+        Integer actual = Math.toIntExact(expectedTestMap.get(1));
+        Integer expected = 50;
+
+        Assert.assertEquals(expected, actual);
+        expectedTestMap.get(49);
+    }
+
+    @Test
+    public void size() {
+        Assert.assertEquals(1, expectedTestMap.size());
     }
 }
